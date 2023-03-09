c2fda58629da87cd0bd6c0289abf096dfccfbc33
Adnan Bayu Aji <adnan.bayuaji@gmail.com>
2023-03-09 21:06:37 +0700

    revisi ya

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ func main() {
 	var j bool = true
 	fmt.Printf("%t \n\n", j)
 	fmt.Printf("%b \n", i)
+	fmt.Printf("%c\n", '\u042F')
 	fmt.Printf("%d \n", 21)
 	fmt.Printf("%o \n", 21)
 	fmt.Printf("%x \n", 15)
 	fmt.Printf("%X \n", 15)
-	fmt.Printf("%U \n", 'Я')
+	fmt.Printf("%U \n\n", 'Я')
 	var k float64 = 123.456
 	fmt.Printf("%.6f \n", k)
 	fmt.Printf("%E \n", k)
